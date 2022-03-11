b085f16307c369e0d70ba12802e64b8be0a6eb8c
Jannik Maag <jannikmaag@outlook.com>
2022-03-11 22:27:16 +0100

    Get options page to read and write local storage

--- src/options-page/OptionsPageApp.vue
+++ src/options-page/OptionsPageApp.vue
@@ -1,89 +1,113 @@
 <template>
-  <div class="options-page">
-    <!-- JIRA URL -->
-    <label for="jira-url">JIRA server url</label>
+  <div class="options">
+    <label for="jira-url">Your JIRA Server Url</label>
+
     <input
       class="u-full-width"
-      v-model="path"
+      v-model="jiraUrl.value"
       type="text"
       placeholder="https://jira.atlassian.yourcompany.net"
       id="jira-url"
     />
-    <!-- COLOR THEME SELECTOR
-    <label for="color-theme">Color theme</label>
-    <select class="u-full-width" id="color-theme">
-      <option value="Default">Light (default)</option>
-      <option value="Dark">Dark</option>
-    </select> -->
 
-    <!-- OVERVIEW PAGE JQL QUERY -->
-    <label for="overview-jql">Issues to show in overview (JQL)</label>
+    <label for="overview-jql">Issues to show in the dashboard (JQL)</label>
+
     <textarea
       class="u-full-width"
+      v-model="jql.value"
       placeholder="STATUS NOT IN ('CLOSED', 'DONE', 'CANCELLED')"
       id="overview-jql"
     ></textarea>
-    <label>
+
+    <!-- <label>
       <input type="checkbox" />
       <span class="label-body">Only query issues assigned to you</span>
-    </label>
-    <!-- SAVE CHANGES -->
-    <button @click.prevent="saveOptions" class="options-page__button">
+    </label> -->
+
+    <button @click.prevent="saveStorageData()" class="options__button">
       Save changes
     </button>
+    <p class="options__info-message" v-if="infoMessage">
+      {{ infoMessage }}
+    </p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
+
+import { PREFIXES as PREFIX_ENUMS } from '@/constants/prefixes'
+
+declare type StorageItem = Record<string, any>
 
 export default defineComponent({
-  name: "OptionsPageApp",
+  name: 'OptionsPageApp',
 
   data() {
     return {
-      storageName: "littleJiraSettings",
-      path: "",
-    };
+      jiraUrl: {
+        storageKey: `${PREFIX_ENUMS.APP_PREFIX}jiraUrl`,
+        value: '',
+      },
+      jql: {
+        storageKey: `${PREFIX_ENUMS.APP_PREFIX}jql`,
+        value: '',
+      },
+      infoMessage: '',
+    }
   },
 
   mounted() {
-    this.restoreOptions();
+    this.restoreStorageData()
   },
-
-  // computed: {},
-
-  // created() {},
 
   methods: {
-    restoreOptions() {
-      // Uncaught (in promise) Error: The storage API will not work with a temporary addon ID. Please add an explicit addon ID to your manifest. For more information see https://mzl.la/3lPk1aE.
-      /* eslint-disable-next-line */
-      const storageItem: { [index: string]: any } = browser.storage.managed.get(
-        this.storageName
-      );
-      this.path = storageItem[this.storageName];
+    getLocalStorage: browser.storage.local.get,
+    setLocalStorage: browser.storage.local.set,
+
+    restoreStorageData() {
+      const keys = [this.jiraUrl.storageKey, this.jql.storageKey]
+
+      this.getLocalStorage(keys)
+        .then((storageData) => {
+          this.jiraUrl.value = storageData[this.jiraUrl.storageKey]
+          this.jql.value = storageData[this.jql.storageKey]
+        })
+        .catch((error) => {
+          this.infoMessage = error
+        })
     },
 
-    saveOptions() {
-      /* eslint-disable-next-line */
-      browser.storage.sync.set({
-        [this.storageName]: this.path,
-      });
+    saveStorageData() {
+      this.setLocalStorage({
+        [this.jiraUrl.storageKey]: this.jiraUrl.value,
+        [this.jql.storageKey]: this.jql.value,
+      })
+        .then(() => {
+          this.infoMessage = 'Settings saved!'
+        })
+        .catch((error) => {
+          this.infoMessage = error
+        })
     },
   },
-});
+})
 </script>
 
 <style lang="scss" scoped>
-@import "../styles/normalize.css";
-@import "../styles/skeleton.css";
+@import '../styles/normalize.css';
+@import '../styles/skeleton.css';
 
-.options-page {
-  padding: 10px 4px;
+.options {
+  background: #f6f6f6;
+  padding: 16px;
 
   &__button {
-    cursor: pointer;
+    background: white;
+  }
+
+  &__info-message {
+    margin: unset;
   }
 }
 </style>
